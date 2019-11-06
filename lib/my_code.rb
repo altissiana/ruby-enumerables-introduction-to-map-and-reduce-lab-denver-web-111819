8d0243b585df4e9a515ea81849e8c9d278b2aeb3
Tissiana Alves <altissiana@gmail.com>
2019-11-06 18:22:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,12 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  
+  new_array = []
+  source_array.each do |number|
+    new_number = number * 2
+    new_array.push(new_number)
+  end
+  new_array
 end  
   
 #map_to_square(source_array)
