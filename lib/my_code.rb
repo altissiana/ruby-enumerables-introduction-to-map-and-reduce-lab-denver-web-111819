50eeeeb3d879c7d8494bd056aa6cfc2dde9169f1
Tissiana Alves <altissiana@gmail.com>
2019-11-06 18:17:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,5 @@
 require 'pp'
 
-
 def map_to_negativize(source_array)
   new_array = []
   source_array.each do |number|
@@ -12,6 +11,9 @@ end
 
 
 
-#map_to_no_change(source_array)
+def map_to_no_change(source_array)
+  
+end
+
 #map_to_double(source_array)
 #map_to_square(source_array)
