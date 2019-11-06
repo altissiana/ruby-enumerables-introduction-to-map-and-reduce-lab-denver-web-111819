8769e08ffdf31c3c73437e1cd720e2079cb82ab2
Tissiana Alves <altissiana@gmail.com>
2019-11-06 19:10:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,11 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  
+  total = 0 
+  for i in starting_point...source_array.length
+  total += source_array[i]
+  end
+  total
 end  
 
 
