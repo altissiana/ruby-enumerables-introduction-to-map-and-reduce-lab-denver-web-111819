1d88b57958cc05cc0201dfdde6fed49cb443c4a1
Tissiana Alves <altissiana@gmail.com>
2019-11-06 19:24:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,9 +40,11 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  source.array do |x|
-    if x[i] == true
-      x = true
+  source_array.each do |x|
+    if x == true
+    
+    else == false
+      
   end  
   x
 end  
